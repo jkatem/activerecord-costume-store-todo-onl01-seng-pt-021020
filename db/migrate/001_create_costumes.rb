5b84ddcfe556dbb6510bb04a8d18d1524349221c
jkatem <jenmiao@gmail.com>
2020-04-29 18:06:54 -0700

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,6 +12,9 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
     def change 
         create_table :costumes do |t|
             t.string :name
+            t.integer :price
+            t.url :image_url
+            t.string :size
         end
     end
 end
