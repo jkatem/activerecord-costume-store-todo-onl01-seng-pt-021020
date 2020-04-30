e8dbe89a5e4468f9a55c9e618408e46b408cc7de
jkatem <jenmiao@gmail.com>
2020-04-29 18:26:08 -0700

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,14 @@
 # Create your costume_stores migration here
 class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change 
+    create_table :costumestore do |t|
+        t.string :name
+        t.string :location
+        t.integer :costume_inventory
+        t.integer :num_of_employees
+        t.boolean :still_in_business
+        t.timestamps
+    end
+end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,11 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
+    t.integer "price"
+    t.string "image_url"
+    t.integer "size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
